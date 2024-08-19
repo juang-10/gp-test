907b4cc4508c01d23a9448ab772e6ebb71cdc95c
david <judagosa8520@gmail.com>
2024-08-19 18:19:36 -0500

    feat: Implement data grid in Home page
    Add a data grid component to the Home page, displaying a list of people. The data grid is configured with pagination, auto height, and a fixed page size of 5. This enhancement improves the user experience by providing a visually appealing and interactive way to view and navigate through the list of people.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,35 @@
+import { People } from '@/data';
+import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+
+
 const Home = () => {
+  const pageSize = 5;
+  const columns: GridColDef<(typeof People)[number]>[] = [{
+    field: 'name',
+    headerName: 'Name',
+    flex: 1,
+    minWidth: 150,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+  }]
+
   return (
-    <div>Home</div>
+    <div>
+      <DataGrid
+        rows={People}
+        columns={columns}
+        disableColumnSelector
+        disableRowSelectionOnClick
+        autoHeight
+        pageSizeOptions={[pageSize]}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: pageSize,
+            },
+          },
+        }}
+      />
+    </div>
   )
 }
 export default Home
