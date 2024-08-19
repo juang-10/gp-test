6670d39b2311b9a6fff447e215aebfd4618e34b6
david <judagosa8520@gmail.com>
2024-08-19 18:17:57 -0500

    fix: Update Navbar position to "fixed"
    The Navbar component in the Navbar.tsx file had its position set to "static" instead of "fixed". This commit fixes the issue by updating the position to "fixed".

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material"
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Gentleman programming React TEST
